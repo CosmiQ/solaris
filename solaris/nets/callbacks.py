00aa567e83f4653a12303d1d0872c3bc5df9c4f6
nrweir <nicholas.r.weir@gmail.com>
2019-05-14 12:25:57 -0700

    fixing rio-tiler dependency

--- solaris/nets/callbacks.py
+++ solaris/nets/callbacks.py
@@ -49,11 +49,6 @@ def get_callbacks(framework, config):
     return callbacks
 
 
-def get_callbacks(framework, config):
-    """Load callbacks based on a config file for a specific framework."""
-    pass  # TODO: IMPLEMENT
-
-
 def terminate_on_metric_nan(model, metric):
     """Get a terminate on metric NaN callback for `metric` in `model`.
 
