ed57ecd999dcc31f472ca886e851d6a7613e1129
Nick Weir <nrweir@users.noreply.github.com>
2019-05-23 09:26:35 -0400

    Complete inference functionality (#89)
    * submodule access with import solaris; adding InferenceTiler
    
    * adding stitching and tests
    
    * triggering build
    
    * removing a to trigger another build
    
    * completing first draft of inference functionality
    
    * debugging InferenceTiler test

--- tests/test_raster/test_image.py
+++ tests/test_raster/test_image.py
@@ -32,8 +32,8 @@ class TestStitchImages(object):
 
     def test_stitch_InferenceTiler_output(self):
         inf_tiler = sol.nets.datagen.InferenceTiler(width=250, height=250)
-        tiles, tile_inds = inf_tiler(os.path.join(data_dir,
-                                                  'sample_geotiff.tif'))
+        tiles, tile_inds, _ = inf_tiler(os.path.join(data_dir,
+                                                     'sample_geotiff.tif'))
         restored_im = stitch_images(tiles, idx_refs=tile_inds,
                                     out_width=900, out_height=900)
         expected_result = sol.utils.io.imread(
@@ -43,8 +43,8 @@ class TestStitchImages(object):
 
     def test_stitch_firstval(self):
         inf_tiler = sol.nets.datagen.InferenceTiler(width=250, height=250)
-        tiles, tile_inds = inf_tiler(os.path.join(data_dir,
-                                                  'sample_geotiff.tif'))
+        tiles, tile_inds, _ = inf_tiler(os.path.join(data_dir,
+                                                     'sample_geotiff.tif'))
         tiles[11, :, :, :] = tiles[11, :, :, :] + 10  # to have a diff to check
         result = stitch_images(tiles, idx_refs=tile_inds,
                                out_width=900, out_height=900, method='first')
@@ -59,7 +59,7 @@ class TestStitchImages(object):
         src_im[src_im != 0] = 1
         src_im = src_im.astype('float64')
         inf_tiler = sol.nets.datagen.InferenceTiler(width=250, height=250)
-        tiles, tile_inds = inf_tiler(src_im)
+        tiles, tile_inds, _ = inf_tiler(src_im)
         rands = np.array([0.93794284, 0.88778908, 0.25066594, 0.76800494,
                           0.43465608, 0.69903218, 0.13256956, 0.20246324,
                           0.65134984, 0.98667763, 0.38168734, 0.85653983,
