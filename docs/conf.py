425a5fd8316ae6a4d905211d02dcff505eb5396a
nrweir <nrweir@users.noreply.github.com>
2019-05-29 14:35:50 -0400

    stop trying to import solaris

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,6 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import solaris
 
 import sphinx_bootstrap_theme
 # -- Project information -----------------------------------------------------
