1fddb09f769647660fe6cae349eb5a9e3cb9a288
jshermeyer <jss5102@gmail.com>
2019-09-10 09:48:42 -0400

    Update __init__.py

--- solaris/nets/zoo/__init__.py
+++ solaris/nets/zoo/__init__.py
@@ -11,26 +11,22 @@ from .multiclass_segmentation import MultiClass_LinkNet34
 
 model_dict = {
     'xdxd_spacenet4': {
-        'weight_path': os.path.join(weights_dir,
-                                    'xdxd_spacenet4_solaris_weights.pth'),
+        'weight_path': os.path.join(weights_dir, 'xdxd_spacenet4_solaris_weights.pth'),
         'weight_url': 'https://s3.amazonaws.com/spacenet-dataset/spacenet-model-weights/spacenet-4/xdxd_spacenet4_solaris_weights.pth',
         'arch': XDXD_SpaceNet4_UNetVGG16
         },
     'selimsef_spacenet4_resnet34unet': {
-        'weight_path': os.path.join(
-            weights_dir, 'selimsef_spacenet4_resnet34unet_solaris_weights.pth'),
+        'weight_path': os.path.join(weights_dir, 'selimsef_spacenet4_resnet34unet_solaris_weights.pth'),
         'weight_url': 'https://s3.amazonaws.com/spacenet-dataset/spacenet-model-weights/spacenet-4/selimsef_spacenet4_resnet34unet_solaris_weights.pth',
         'arch': SelimSef_SpaceNet4_ResNet34UNet
         },
     'selimsef_spacenet4_densenet121unet': {
-        'weight_path': os.path.join(
-            weights_dir, 'selimsef_spacenet4_densenet121unet_solaris_weights.pth'),
+        'weight_path': os.path.join(weights_dir, 'selimsef_spacenet4_densenet121unet_solaris_weights.pth'),
         'weight_url': 'https://s3.amazonaws.com/spacenet-dataset/spacenet-model-weights/spacenet-4/selimsef_spacenet4_densenet121unet_solaris_weights.pth',
         'arch': SelimSef_SpaceNet4_DenseNet121UNet
         },
     'selimsef_spacenet4_densenet161unet': {
-        'weight_path': os.path.join(
-            weights_dir, 'selimsef_spacenet4_densenet161unet_solaris_weights.pth'),
+        'weight_path': os.path.join(weights_dir, 'selimsef_spacenet4_densenet161unet_solaris_weights.pth'),
         'weight_url': 'https://s3.amazonaws.com/spacenet-dataset/spacenet-model-weights/spacenet-4/selimsef_spacenet4_densenet161unet_solaris_weights.pth',
         'arch': SelimSef_SpaceNet4_DenseNet161UNet
         },
