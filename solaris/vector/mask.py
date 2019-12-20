6d84d2f6542c77b43a6e709e4a0f2bc478ef2efa
Ryan Avery <ravery@ucsb.edu>
2019-12-20 12:07:44 -0800

    accept try/except change
    Co-Authored-By: Nick Weir <nrweir@users.noreply.github.com>

--- solaris/vector/mask.py
+++ solaris/vector/mask.py
@@ -949,7 +949,10 @@ def instance_mask(df, out_file=None, reference_im=None, geom_col='geometry',
     if reference_im:
         reference_im = _check_rasterio_im_load(reference_im)
         try:
+    try:
             bad_data_mask = (reference_im.read() == reference_im.nodata).any(axis=0) # take logical and along all dims so that all pixxels not -9999 across bands
+    except AttributeError as ae:  # raise another, more verbose AttributeError
+        raise AttributeError("A nodata value is not defined for the source image. Make sure the reference_im has a nodata value defined.") from ae
         except AttributeError:
             print("A nodata value is not defined for the source image. Make sure the reference_im has a nodata value defined.")
             sys.exit(1)
