4f402bf93f4bc87f2b9206d8d947e778e8947682
Ryan Avery <ravery@ucsb.edu>
2019-12-20 02:57:32 +0000

    fix axis issues, labels converted to mask only where image values are not equal to nodata

--- solaris/vector/mask.py
+++ solaris/vector/mask.py
@@ -949,11 +949,12 @@ def instance_mask(df, out_file=None, reference_im=None, geom_col='geometry',
     if reference_im:
         reference_im = _check_rasterio_im_load(reference_im)
         try:
-            bad_data_mask = (reference_im.read() != reference_im.nodata).all(axis=0) # take logical and along all dims so that all pixxels not -9999 across bands
+            bad_data_mask = (reference_im.read() == reference_im.nodata).any(axis=0) # take logical and along all dims so that all pixxels not -9999 across bands
         except AttributeError:
             print("A nodata value is not defined for the source image. Make sure the reference_im has a nodata value defined.")
             sys.exit(1)
-        output_arr = output_arr.where(bad_data_mask) # mask is broadcasted to filter labels where there are non-nan image values
+        bad_data_mask = np.dstack([bad_data_mask]*output_arr.shape[2])
+        output_arr = np.where(bad_data_mask, 0, output_arr) # mask is broadcasted to filter labels where there are non-nan image values
 
     if out_file:
         meta = reference_im.meta.copy()
