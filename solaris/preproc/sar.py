fe7d6601daa9a21aa530f30cb9c1911a9a6342bd
Daniel Hogan <6313241+dphogan@users.noreply.github.com>
2020-04-11 23:07:18 +0000

    Combine Multilook and UnequalMultilook into one more flexible class

--- solaris/preproc/sar.py
+++ solaris/preproc/sar.py
@@ -7,64 +7,28 @@ from .image import Image
 
 
 class Multilook(PipeSegment):
+    """
+    Multilook filter to reduce speckle in SAR magnitude imagery
+    Note: Set kernel_size to a tuple to vary it by direction.
+    """
     def __init__(self, kernel_size=5, method='avg'):
         super().__init__()
         self.kernel_size = kernel_size
         self.method = method
     def transform(self, pin):
+        if self.method == 'avg':
+            filter = scipy.ndimage.filters.uniform_filter
+        elif self.method == 'med':
+            filter = scipy.ndimage.filters.median_filter
+        elif self.method == 'max':
+            filter = scipy.ndimage.filters.maximum_filter
+        else:
+            raise Exception('! Invalid method in Multilook.')
         pout = Image(np.zeros(pin.data.shape, dtype=pin.data.dtype),
                      pin.name, pin.metadata)
         for i in range(pin.data.shape[0]):
-            if self.method =='avg':
-                pout.data[i, :, :] = scipy.signal.convolve(
-                    pin.data[i, :, :],
-                    np.ones((self.kernel_size,
-                             self.kernel_size)) / (self.kernel_size**2),
-                    mode='same')
-            elif self.method == 'med':
-                pout.data[i, :, :] = scipy.ndimage.filters.median_filter(
-                    pin.data[i, :, :],
-                    self.kernel_size,
-                    mode='reflect')
-            elif self.method == 'max':
-                pout.data[i, :, :] = scipy.ndimage.filters.maximum_filter(
-                    pin.data[i, :, :],
-                    self.kernel_size,
-                    mode='reflect')
-            elif self.method == 'non':
-                pass
-            else:
-                raise Exception('! Invalid method in Multilook.')
-        return pout
-
-
-class UnequalMultilook(PipeSegment):
-    def __init__(self, usize=5, vsize=5, method='avg'):
-        super().__init__()
-        self.usize = usize
-        self.vsize = vsize
-        self.method = method
-    def transform(self, pin):
-        pout = Image(np.zeros(pin.data.shape, dtype=pin.data.dtype),
-                     pin.name, pin.metadata)
-        for i in range(pin.data.shape[0]):
-            if self.method =='avg':
-                pout.data[i, :, :] = scipy.ndimage.filters.uniform_filter(
-                    pin.data[i, :, :],
-                    size=(self.usize, self.vsize),
-                    mode='reflect')
-            elif self.method == 'med':
-                pout.data[i, :, :] = scipy.ndimage.filters.median_filter(
-                    pin.data[i, :, :],
-                    size=(self.usize, self.vsize),
-                    mode='reflect')
-            elif self.method == 'max':
-                pout.data[i, :, :] = scipy.ndimage.filters.maximum_filter(
-                    pin.data[i, :, :],
-                    size=(self.usize, self.vsize),
-                    mode='reflect')
-            elif self.method == 'non':
-                pass
-            else:
-                raise Exception('! Invalid method in Multilook.')
+            pout.data[i, :, :] = filter(
+                pin.data[i, :, :],
+                size=self.kernel_size,
+                mode='reflect')
         return pout
