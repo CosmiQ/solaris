679db13ed050afbf8fd52f733c24f14afc8131b8
Daniel Hogan <6313241+dphogan@users.noreply.github.com>
2020-06-04 03:32:55 +0000

    Three classes for working with GeoPandas DataFrames

--- solaris/preproc/label.py
+++ solaris/preproc/label.py
@@ -1,3 +1,5 @@
+import geopandas as gpd
+
 from .pipesegment import PipeSegment, LoadSegment, MergeSegment
 
 
@@ -38,3 +40,34 @@ class ShowString(PipeSegment):
     def transform(self, pin):
         print(pin)
         return pin
+
+
+class LoadDataFrame(LoadSegment):
+    """
+    Load a GeoPandas dataframe from a file.
+    """
+    def __init__(self, pathstring):
+        super().__init__()
+        self.pathstring = pathstring
+    def process(self):
+        return gpd.read_file(self.pathstring)
+
+
+class ExplodeDataFrame(PipeSegment):
+    """
+    Given a GeoPandas DataFrame, break multi-part geometries
+    into multiple lines.
+    """
+    def transform(self, pin):
+        return pin.explode().reset_index()
+
+
+class DataFrameToString(PipeSegment):
+    """
+    Given a GeoPandas DataFrame, convert it into a GeoJSON string.
+    """
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.kwargs = kwargs
+    def transform(self, pin):
+        return pin.to_json(**(self.kwargs))
