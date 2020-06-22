61de3405c22f58fce520198073371f59c012c667
roshanr11 <roshan.ram0101@gmail.com>
2020-06-22 15:40:37 -0400

    testing original file once more

--- solaris/nets/train.py
+++ solaris/nets/train.py
@@ -217,11 +217,12 @@ class Trainer(object):
 
             elif isinstance(cb, TorchModelCheckpoint):
                 if cb.monitor == 'loss':
-                    cb(self.model, loss_value=loss) 
+                    cb(self.model, loss_value=loss)
                 elif cb.monitor == 'val_loss':
                     cb(self.model, loss_value=val_loss)
                 elif cb.monitor == 'periodic':
-                    cb(self.model, period=self.config['training']['checkpoint_frequency']) # THIS LINE IS THE ERROR -- I THINK I FIXED IT
+                    # cb(self.model, period=self.config['training']['checkpoint_frequency']) # THIS LINE IS THE ERROR -- I THINK I FIXED IT
+                    cb(self.model)
 
         return True
 
