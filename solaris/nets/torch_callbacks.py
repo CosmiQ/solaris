e86e9c6bb33a6f81cb51f857ada527e05ef5b517
roshanr11 <roshan.ram0101@gmail.com>
2020-06-23 16:42:57 -0400

    Update torch_callbacks.py

--- solaris/nets/torch_callbacks.py
+++ solaris/nets/torch_callbacks.py
@@ -180,7 +180,7 @@ class TorchModelCheckpoint(object):
             if self.last_epoch + self.period <= self.epoch:
                 self.last_saved_value = loss_value if loss_value else 0
                 self.save(model, self.weights_only)
-                self.last_epoch = self.epoch
+                self.last_epoch = self.epoch 
 
 
         elif self.monitor in ['loss', 'val_loss']:
