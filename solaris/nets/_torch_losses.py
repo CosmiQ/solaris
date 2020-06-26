Incorrect focal loss computation for test purposes

solaris/nets/_torch_losses.py
@@ -65,10 +65,10 @@ class TorchFocalLoss(nn.Module):
 
         if self.logits:
             BCE_loss = F.binary_cross_entropy_with_logits(outputs, targets,
-                                                          reduction='mean')
+                                                          reduction='sum')
         else:
             BCE_loss = F.binary_cross_entropy(outputs, targets,
-                                              reduction='mean')
+                                              reduction='sum')
         pt = torch.exp(-BCE_loss)
         F_loss = (1-pt)**self.gamma * BCE_loss
         if self.reduce: