998488a84f238912192c1fabc6ce9281b88f2126
jshermeyer <jss5102@gmail.com>
2020-07-08 15:31:13 -0400

    Expanded metrics v3 (#381)
    * expanded_metrics bug fix
    
    * Update vector_test.py
    
    Co-authored-by: Jacob Shermeyer <jshermeyer@labs-lt-6518.labs.internal>

--- tests/test_eval/vector_test.py
+++ tests/test_eval/vector_test.py
@@ -9,5 +9,5 @@ class TestVectorMetrics(object):
     def test_vector_metrics(self):
         proposal_polygons_dir = os.path.join(data_dir, "eval_vector/preds/")
         gt_polygons_dir = os.path.join(data_dir, "eval_vector/gt/")
-        mF1, f1s_by_class, precision_iou_by_obj, precision_by_class, mPrecision, recall_iou_by_obj, recall_by_class, mRecall, object_subset = vector.mF1(proposal_polygons_dir, gt_polygons_dir, prediction_cat_attrib="class", gt_cat_attrib='make', all_outputs=True)
-        assert mF1.round(2) == 0.73
+        mF1_score, f1s_by_class, precision_iou_by_obj, precision_by_class, mPrecision, recall_iou_by_obj, recall_by_class, mRecall, object_subset, gt_preds, poly_preds = vector.mF1(proposal_polygons_dir, gt_polygons_dir, prediction_cat_attrib="class", gt_cat_attrib='make', all_outputs=True)
+        assert mF1_score.round(2) == 0.83
