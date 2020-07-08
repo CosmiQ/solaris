8dbd43c6fb629fe08395ee6e3dd2f9719afe7273
Daniel Hogan <6313241+dphogan@users.noreply.github.com>
2020-07-08 03:36:03 +0000

    readthedocs build error fix

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 import sphinx_bootstrap_theme
+import numpy as np
 # -- Project information -----------------------------------------------------
 
 project = 'solaris'
