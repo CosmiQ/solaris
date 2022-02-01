0e52484f05359b6c84317bf67517966d98d2a22f
SRM <soumya@developmentseed.org>
2022-02-01 19:06:48 +0530

    update requests & opencv

--- solaris/__init__.py
+++ solaris/__init__.py
@@ -1,4 +1,4 @@
-from . import data, utils, vector
+from . import data, utils, vector, tile
 
 #  data, eval, preproc, raster, tile, have gdal in them need to replace with rasterio
 __version__ = "0.0.1"
